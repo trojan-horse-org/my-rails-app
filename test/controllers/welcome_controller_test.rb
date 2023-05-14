d8600c03670feff44909ae42c813c4fa4dbb6b59
Gus SS <gusshawstewart@github.com>
2023-05-14 19:11:55 +0100

    Update WelcomeControllerTest

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get root_url
+    get welcome_index_url
     assert_response :success
   end
 end
